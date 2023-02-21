4203dc659d01ae962d6f59bfce73d94107965d67
Nikhil <Naiknikhil925@gmail.com>
2023-02-22 00:28:36 +0530

    updated home page

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,6 +5,8 @@ import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
 import { filterData } from "./shared/helper";
 import CarouselCard from "./CarouselCard";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 const Body = () => {
   const [allRestaurants, setAllRestaurants] = useState([]);
@@ -47,33 +49,26 @@ const Body = () => {
       </div>
 
       <div className=" flex items-center  mx-auto p-3 ">
-        <input
-          type="text"
-          className=" w-96 p-2  text-md  border border-gray-300 rounded-2xl bg-gray-50 focus:ring-blue-500  dark:bg-purple-50 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black "
-          placeholder="Search..."
-          value={searchText}
-          onChange={(e) => {
-            setSearchText(e.target.value);
-            const data = filterData(e.target.value, allRestaurants);
-            {
-              console.log(data);
-            }
-            setFilteredRes(data);
-          }}
-        />
-        <div className="p-2 ">
-          <svg
-            className="w-10 h-10  cursor-pointer"
-            fill="currentColor"
-            viewBox="0 0 24 24"
-            aria-hidden="true"
-          >
-            <path
-              d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
-              fill="#000000"
-            ></path>{" "}
-          </svg>
+        <div className=" w-96 p-2  text-md  border border-gray-300 rounded-2xl bg-gray-50 focus:ring-blue-500  dark:bg-purple-50 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black ">
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+          <input
+            type="text"
+            className="outline-none dark:bg-purple-50 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black p-1"
+            placeholder="Search..."
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+              const data = filterData(e.target.value, allRestaurants);
+              {
+                console.log(data);
+              }
+              setFilteredRes(data);
+            }}
+          />
         </div>
+      </div>
+      <div className="font-bold text-2xl ml-36">
+        <p>{allRestaurants.length} Restaurants Near You</p>
       </div>
 
       <div className=" px-11 flex flex-wrap justify-center">
