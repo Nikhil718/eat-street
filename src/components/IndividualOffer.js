878fb81f1589c27183bfda22d0267b41f68af58f
Nikhil <Naiknikhil925@gmail.com>
2023-02-21 23:32:43 +0530

    updated offers page

--- src/components/IndividualOffer.js
+++ src/components/IndividualOffer.js
@@ -7,9 +7,9 @@ const IndividualOffer = ({
   description,
   validTill,
 }) => {
-  return (
+  return logo == null ? null : (
     <div className="w-64 h-64 p-8 m-3 border-2 border-inherit hover:shadow-2xl rounded-sm ">
-      <div className="flex bg-pink-100 rounded-lg p-2">
+      <div className="flex bg-pink-100 rounded-lg p-2 justify-center">
         <img className="w-5" src={OFFER_IMG_URL + logo} />
         <h1 className="m-1">{couponCode}</h1>
       </div>
